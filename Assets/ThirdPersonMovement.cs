a6dd273b7300d4303eca765e6f8313903f55df74
Katu <captain.katu@gmail.com>
2023-01-03 15:28:13 -0600

    playing with movement
    Attempting to use a different script for movement - thirdpersonmovement.cs instead of playercontroller.cs. bonuses,  it now accepts camera angle input.
    
    still need to smooth out some camera control settings.
    
    Unfortunately cannot work out how to jump using the character controller instead of rigidbody. will mess with it more later.

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
+
     public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     // Update is called once per frame
     void Update()
@@ -13,13 +18,18 @@ public class ThirdPersonMovement : MonoBehaviour
         //Get wasd or arrow key input and make a new Vector3 using tat info.
         //Add "normalized" to make sure that moving diagonally doesn't double
         //speed because you're pressing 2 keys at once.
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = UnityEngine.Input.GetAxisRaw("Horizontal");
+        float vertical = UnityEngine.Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
